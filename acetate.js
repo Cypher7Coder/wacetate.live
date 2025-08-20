45b24bf870d5f32389beec2145ed71e39662a1bd
Francisco Mendoza <seecware@gmail.com>
2025-08-20 15:05:21 -0600

    Main code to inject into browser console.
    Just like that.

---
+++ acetate.js
@@ -0,0 +1,48 @@
+// Crear overlay
+let overlay = document.createElement('div');
+Object.assign(overlay.style, {
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  width: '100%',
+  height: '100%',
+  pointerEvents: 'none',
+  zIndex: '0'
+});
+document.body.appendChild(overlay);
+
+let img = document.createElement('img');
+img.src = 'https://avatars.githubusercontent.com/u/179284048?v=4';
+Object.assign(img.style, {
+  width: '600px',
+  height: '600px',
+  position: 'absolute',
+  top: '150px',
+  left: '150px',
+  cursor: 'grab',
+  userSelect: 'none',
+  opacity: '0.3',     
+  pointerEvents: 'none'
+});
+overlay.appendChild(img);
+
+let dragging = false, offsetX = 0, offsetY = 0;
+
+img.addEventListener('mousedown', e => {
+  dragging = true;
+  offsetX = e.clientX - img.getBoundingClientRect().left;
+  offsetY = e.clientY - img.getBoundingClientRect().top;
+  img.style.pointerEvents = 'auto';
+});
+
+document.addEventListener('mousemove', e => {
+  if (!dragging) return;
+  img.style.left = (e.clientX - offsetX) + 'px';
+  img.style.top = (e.clientY - offsetY) + 'px';
+});
+
+document.addEventListener('mouseup', () => {
+  dragging = false;
+  img.style.pointerEvents = 'none';
+});
+
